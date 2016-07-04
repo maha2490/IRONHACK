5e78bd010d4e7bfbd77495438ac5d91dcac4d501
Rachel Mahan <rachelm2490@gmail.com>
2016-07-04 11:10:28 -0400

    Time tracker, commit 1

--- course/week_4/time_tracking/config/routes.rb
+++ course/week_4/time_tracking/config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+# sends to site controller & into the methods there
+  get "/", to: "site#home"
+  get "/contact", to: "site#contact"
 end
