82260135946d97c7f91cfd8ece76b151dd7fca7d
Rachel Mahan <rachelm2490@gmail.com>
2016-07-06 14:56:59 -0400

    Time tracker, commit 4

--- course/week_4/time_tracking/config/routes.rb
+++ course/week_4/time_tracking/config/routes.rb
@@ -3,8 +3,9 @@ Rails.application.routes.draw do
   
   get "/", to: "site#home"
   get "/contact", to: "site#contact"
-  get"/projects", to: "projects#index"
-  get"/projects/new", to: "projects#new"
+  get "/projects", to: "projects#index"
+  get "/projects/new", to: "projects#new"
   post "/projects", to: "projects#create"
-  get"/projects/:id", to: "projects#show"
+  get "/projects/:id", to: "projects#show"
+  get "/projects/:project_id/time_entries", to: "time_entries#index"
 end
