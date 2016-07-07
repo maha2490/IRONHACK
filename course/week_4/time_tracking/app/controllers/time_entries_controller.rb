47d8c65c7f6f87437a73c30e8f8e3da5d7fa6c60
Rachel Mahan <rachelm2490@gmail.com>
2016-07-07 14:59:05 -0400

    Time tracker, commit 6

--- course/week_4/time_tracking/app/controllers/time_entries_controller.rb
+++ course/week_4/time_tracking/app/controllers/time_entries_controller.rb
@@ -14,18 +14,14 @@ class TimeEntriesController < ApplicationController
 	def create
 		@my_project = Project.find(params[:project_id])
 
-		@my_entry = @my_project.time_entries.new(
-			:hours => params[:time_entry][:hours],
-			:minutes => params[:time_entry][:minutes],
-			:date => params[:time_entry][:date],
-			:comments => params[:time_entry][:comments])
-
-		# another way to write the above
-		# @my_entry = TimeEntry.new(
-		# 	project_id: @my_project.id, 
-		# 	hours: params[:time_entry][:hours]
-		# 	other params ..
-		# 	)
+		@my_entry = @my_project.time_entries.new(entry_params)
+		
+				# another way to write the above
+				# @my_entry = TimeEntry.new(
+				# 	project_id: @my_project.id, 
+				# 	hours: params[:time_entry][:hours]
+				# 	other params ..
+				# 	)
 
 		if @my_entry.save  #.save only returns true/false
 						   # makes sure all info passed validation
@@ -35,5 +31,43 @@ class TimeEntriesController < ApplicationController
 		end
 	end
 
+	def edit
+		@my_project = Project.find params[:project_id]
+		@my_entry = @my_project.time_entries.find params[:id]
+		render "edit"
+	end
+
+	def update
+		@my_project = Project.find_by(id: params[:project_id])
+		@my_entry = @my_project.time_entries.find_by(id: params[:id])
+
+		if @my_entry.update(entry_params)
+					# entry_params replaces |
+					# 					  v		
+					# 		hours: params[:time_entry][:hours],
+					# 		minutes: params[:time_entry][:minutes],
+					# 		date: params[:time_entry][:date],
+					# 		comments: params[:time_entry][:comments])
+			redirect_to action: "index", controller: "time_entries", project_id: @my_project.id
+		else
+			render "edit"
+		end
+
+	end
+
+	def destroy
+		@my_project = Project.find params[:project_id]
+		@my_entry = @my_project.time_entries.find params[:id]
+		@my_entry.destroy
+
+		redirect_to(project_time_entries_path(@my_project)) 
+	end
+
+	private # a method that states that the following is not a route, all below
+
+	def entry_params
+		params.require(:time_entry).permit(:hours, :minutes, :date, :comments)
+		# if someone submits another form that ins't time_entry, an error is thrown
+	end
 
 end
--- course/week_4/time_tracking/app/models/project.rb
+++ course/week_4/time_tracking/app/models/project.rb
@@ -1,6 +1,11 @@
 class Project < ApplicationRecord
 	validates :name, presence: true, uniqueness: true, length: {maximum: 30}, format: {with: /[a-zA-Z ]/}
 	has_many :time_entries  #plural version of table, auto applied via Rails
+
+	def self.clean_old
+		old_stuff = Project.where("created_at < ?", 1.week.ago) #find projects older than a week
+		old_stuff.destroy_all
+	end
 end
 
 
